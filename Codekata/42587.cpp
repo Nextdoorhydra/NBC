4aaeb03dbad539fe430dade03a22bff3bcd46966
NDH <ndhphysics@gmail.com>
2026-04-22 10:49:24 +0900

    codekata

---
+++ Codekata/42587.cpp
@@ -0,0 +1,56 @@
+// 42587
+// https://school.programmers.co.kr/learn/courses/30/lessons/42587
+#include<iostream>
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+
+int solution(vector<int> priorities, int location) {
+    // ----------init----------
+    struct element {
+        int index, priority;
+    };
+    int i, idx = 0, answer = 1;
+    element e;
+    queue<element> q;
+    priority_queue<int> pq;
+
+    // ----------processing----------
+    for (int x : priorities) q.push(element{ idx++, x });
+    for (int x : priorities) pq.push(x);
+
+    // ----------main----------
+    vector<element> debug;
+
+    while(!q.empty()) {
+        e = q.front();
+        q.pop();
+
+        if (e.priority >= pq.top()) {
+            if (e.index == location)
+                return answer;
+            
+            answer++;
+            pq.pop();
+        }
+        else {
+            q.push(e);
+        }        
+    }
+
+    return answer;
+}
+
+int main(void) {
+    //vector<int> input{ 2, 1, 3, 2 };
+    //int loc = 2;
+    vector<int> input{ 5,4,3,2,1 };
+    int loc = 4;
+
+    printf("%d", solution(input, loc));
+
+    return 0;
+}
