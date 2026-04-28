18c731d76296ab04e3b67bb46ef4748930785c78
NDH <ndhphysics@gmail.com>
2026-04-28 23:11:10 +0900

    Update .gitignore and remove tracked files

--- MyProjectNBC/Intermediate/Build/Win64/x64/MyProjectNBCEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/NDH/NBC/MyProjectNBC/Intermediate/Build/Win64/x64/MyProjectNBCEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
