e08eede8a20f648dbff577597fb896b1d96e701d
NDH <ndhphysics@gmail.com>
2026-04-28 23:11:10 +0900

    Update .gitignore and remove tracked files

--- MyProjectNBC/Intermediate/Build/Win64/x64/MyProjectNBCEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "C:/NDH/NBC/MyProjectNBC/Intermediate/Build/Win64/x64/MyProjectNBCEditor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
