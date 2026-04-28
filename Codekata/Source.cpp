a16785e6b181690bbacf3ae879ce2424e6f55e9d
NDH <ndhphysics@gmail.com>
2026-04-28 09:49:25 +0900

    codekata

--- Codekata/Source.cpp
+++ Codekata/Source.cpp
@@ -1,8 +1,22 @@
-//# include <iostream>
-//using namespace std;
+//#include <iostream> 
 //
-//void main() {
-//    if (int a = 3; a > 5) {
+//int main() {
+//    int* p[5];
 //
+//    int a = 5;
+//
+//    for(int i = 0; i < 5; ++i) {
+//        p[i] = new int;
+//        p[i] = &a;
 //    }
+//
+//    for(int i = 0; i < 5; ++i) {
+//        std::cout << *p[i] << std::endl;
+//    }
+//
+//    for(int i = 0; i < 5; ++i) {
+//        delete p[i];
+//    }
+//
+//    return 0;
 //}
