092d0bf6c00489fd47b0c9054b97690d643ada2f
NDH <ndhphysics@gmail.com>
2026-05-13 09:58:21 +0900

    codekata

--- Codekata/68936.cpp
+++ Codekata/68936.cpp
@@ -6,49 +6,64 @@
 
 using namespace std;
 
-int recur(vector<vector<int>>& arr, pair<int, int> startPos, int sep)
+struct node {
+    int value;
+    vector<node>* next;
+};
+
+int recur(vector<vector<int>>& arr, pair<int, int> startPos, int sep, node answer)
 {
     int c = startPos.first, r = startPos.second, nextSep = sep / 2;
 
-    if (sep == 0)
+    if (sep == 1)
     {
         return arr[r][c];
     }
-    else if (arr[r][c] == 0
-        && arr[r][c + nextSep] == 0
-        && arr[r + nextSep][c] == 0
-        && arr[r + nextSep][c + nextSep] == 0)
+
+
+    int r1 = recur(arr, make_pair(r, c), nextSep, answer);
+    int r3 = recur(arr, make_pair(r + nextSep, c), nextSep, answer);
+    int r2 = recur(arr, make_pair(r, c + nextSep), nextSep, answer);
+    int r4 = recur(arr, make_pair(r + nextSep, c + nextSep), nextSep, answer);
+
+    if (r1 && r2 && r3 && r4)
+    {
+
+        return 1;
+    }
+    else if (!r1 && !r2 && !r3 && !r4)
     {
         return 0;
     }
-    else if (arr[r][c] == 1
-        && arr[r][c + nextSep] == 1
-        && arr[r + nextSep][c] == 1
-        && arr[r + nextSep][c + nextSep] == 1)
-    {
-        return 1;
-    }
-
-    cout << '(' << r << ", " << c << ')' << endl;
-
-    recur(arr, make_pair(r, c), nextSep);
-    recur(arr, make_pair(r, c + nextSep), nextSep);
-    recur(arr, make_pair(r + nextSep, c), nextSep);
-    recur(arr, make_pair(r + nextSep, c + nextSep), nextSep);
 }
 
 vector<int> solution(vector<vector<int>> arr)
 {
     vector<int> answer;
 
-    recur(arr, make_pair(0, 0), arr.size());
+    recur(arr, make_pair(0, 0), arr.size(), node());
 
     return answer;
 }
 
 int main()
 {
-    solution(vector<vector<int>>{{1, 1, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 1}, {1, 1, 1, 1}});
+    //solution(std::vector<std::vector<int>>{
+    //    {1, 1, 1, 1},
+    //    {1, 0, 0, 1},
+    //    {1, 1, 0, 1},
+    //    {1, 1, 1, 1},
+    //    });
+    solution(std::vector<std::vector<int>>{
+    {1, 1, 1, 1, 1, 1, 1, 1},
+    {0, 1, 1, 1, 1, 1, 1, 1},
+    {0, 0, 0, 0, 1, 1, 1, 1},
+    {0, 1, 0, 0, 1, 1, 1, 1},
+    {0, 0, 0, 0, 0, 0, 1, 1},
+    {0, 0, 0, 0, 0, 0, 0, 1},
+    {0, 0, 0, 0, 1, 0, 0, 1},
+    {0, 0, 0, 0, 1, 1, 1, 1}
+        });
 
     return 0;
 }
