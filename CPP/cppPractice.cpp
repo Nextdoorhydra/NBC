a30057ce82c8a137f197c88432e4e8724f00f9f5
NDH <ndhphysics@gmail.com>
2026-05-14 10:14:48 +0900

    codekata

--- CPP/cppPractice.cpp
+++ CPP/cppPractice.cpp
@@ -1,11 +1,11 @@
-//#include <iostream>
-//using namespace std;
+#include <iostream>
+using namespace std;
 //
 //int main() {
 //    int a = 5;
 //    const int& b{ a };
 //
-//
+//    int ¿µ¾î = 5;
 //
 //    return 0;
 //}
