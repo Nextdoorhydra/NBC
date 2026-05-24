db4c7f9e0e901601be2aeffb98ec04646ebf31f7
NDH <ndhphysics@gmail.com>
2026-05-25 01:19:51 +0900

    codekata

--- CPP/cppPractice.cpp
+++ CPP/cppPractice.cpp
@@ -1,1 +1,24 @@
-﻿
+﻿#include <iostream>
+
+using namespace std;
+
+class Base {
+public:
+    virtual void func1();
+    virtual void func2();
+    void nonVirtualFunc();
+};
+
+class Derived : public Base {
+public:    
+    virtual void func2() override;
+    void nonVirtualFunc();
+};
+
+int main()
+{
+    Base myBase;
+    Derived myDerived;
+
+    return 0;
+}
