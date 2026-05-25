e23bc74937ba82e11e9ba7dd48b1906e9e2635ad
NDH <ndhphysics@gmail.com>
2026-05-26 05:10:31 +0900

    codekata 12910

---
+++ Codekata/12910.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor)
+{
+    vector<int> answer;
+
+    for (const auto& n : arr)
+    {
+        if (n % divisor == 0)
+            answer.push_back(n);
+    }
+
+    if (answer.empty())
+        return { -1 };
+
+    sort(answer.begin(), answer.end(), less<int>());
+
+    return answer;
+}
