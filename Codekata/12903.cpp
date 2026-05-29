ce9e7e9e78e9804eada33beed2a697573908366b
NDH <ndhphysics@gmail.com>
2026-05-29 22:43:09 +0900

    12903

--- Codekata/12903.cpp
+++ Codekata/12903.cpp
@@ -3,18 +3,16 @@
 
 using namespace std;
 
-string solution(string s)
+string solution(int n)
 {
-    // 0 -> 1 / 2 = 0
-    // 0 1 -> 2 / 2 = 1
-    // 0 1 2 -> 3 / 2 = 1
-    // 0 1 2 3 4 -> 5 / 2 = 2
-    bool isEven = s.size() % 2 == 0;
-    int len = s.size() / 2 - (isEven ? 1 : 0);
-    return s.substr(len, isEven ? 2 : 1);
-}
+    string answer = "";
+    int cnt = 0;
 
-int main()
-{
-    solution("abcde");
+    while (n-- > 0)
+    {
+        auto s = (n % 2 == 0) ? "수" : "박";
+        answer += s;
+    }
+
+    return answer;
 }
